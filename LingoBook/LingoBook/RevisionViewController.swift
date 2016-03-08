9f5c701284e5f282c007c0a12b1facbb8cf13700
Connor Goddard <hello@connorlukegoddard.com>
2016-03-08 22:48:36 +0000

    Initial commit of project files and image assets.

---
+++ LingoBook/LingoBook/RevisionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LingoBook
+//
+//  Created by Connor Goddard on 08/03/2016.
+//  Copyright © 2016 Connor Goddard. All rights reserved.
+//
+
+import UIKit
+
+class RevisionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
